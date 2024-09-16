24d616154effa3194076590a376050f098c524a2
SemehAmeur <semeh.ameur@gmail.com>
2024-09-16 10:28:55 +0200

    config check data py

---
+++ UI/ui_check_data.py
@@ -0,0 +1,288 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'check_data.ui'
+##
+## Created by: Qt User Interface Compiler version 6.6.0
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QApplication, QGridLayout, QHBoxLayout, QLabel,
+    QPlainTextEdit, QPushButton, QSizePolicy, QSpacerItem,
+    QVBoxLayout, QWidget)
+
+class Ui_CheckData(object):
+    def setupUi(self, CheckData):
+        if not CheckData.objectName():
+            CheckData.setObjectName(u"CheckData")
+        CheckData.resize(862, 565)
+        self.gridLayout_2 = QGridLayout(CheckData)
+        self.gridLayout_2.setObjectName(u"gridLayout_2")
+        self.verticalLayout = QVBoxLayout()
+        self.verticalLayout.setObjectName(u"verticalLayout")
+        self.gridLayout = QGridLayout()
+        self.gridLayout.setObjectName(u"gridLayout")
+        self.birthdateLabel = QLabel(CheckData)
+        self.birthdateLabel.setObjectName(u"birthdateLabel")
+        font = QFont()
+        font.setPointSize(11)
+        self.birthdateLabel.setFont(font)
+
+        self.gridLayout.addWidget(self.birthdateLabel, 2, 5, 1, 1)
+
+        self.websiteLabel = QLabel(CheckData)
+        self.websiteLabel.setObjectName(u"websiteLabel")
+        self.websiteLabel.setFont(font)
+
+        self.gridLayout.addWidget(self.websiteLabel, 0, 1, 1, 1)
+
+        self.linkedWebsiteLabel = QLabel(CheckData)
+        self.linkedWebsiteLabel.setObjectName(u"linkedWebsiteLabel")
+        self.linkedWebsiteLabel.setFont(font)
+
+        self.gridLayout.addWidget(self.linkedWebsiteLabel, 0, 5, 1, 1)
+
+        self.firstNameLabel = QLabel(CheckData)
+        self.firstNameLabel.setObjectName(u"firstNameLabel")
+        self.firstNameLabel.setFont(font)
+
+        self.gridLayout.addWidget(self.firstNameLabel, 3, 1, 1, 1)
+
+        self.label_11 = QLabel(CheckData)
+        self.label_11.setObjectName(u"label_11")
+
+        self.gridLayout.addWidget(self.label_11, 0, 4, 1, 1)
+
+        self.label_17 = QLabel(CheckData)
+        self.label_17.setObjectName(u"label_17")
+
+        self.gridLayout.addWidget(self.label_17, 3, 4, 1, 1)
+
+        self.linkLabel = QLabel(CheckData)
+        self.linkLabel.setObjectName(u"linkLabel")
+        self.linkLabel.setFont(font)
+
+        self.gridLayout.addWidget(self.linkLabel, 0, 3, 1, 1)
+
+        self.lastNameLabel = QLabel(CheckData)
+        self.lastNameLabel.setObjectName(u"lastNameLabel")
+        self.lastNameLabel.setFont(font)
+
+        self.gridLayout.addWidget(self.lastNameLabel, 3, 3, 1, 1)
+
+        self.ansewerLabel = QLabel(CheckData)
+        self.ansewerLabel.setObjectName(u"ansewerLabel")
+        self.ansewerLabel.setFont(font)
+
+        self.gridLayout.addWidget(self.ansewerLabel, 3, 5, 1, 1)
+
+        self.secondMailLabel = QLabel(CheckData)
+        self.secondMailLabel.setObjectName(u"secondMailLabel")
+        self.secondMailLabel.setFont(font)
+
+        self.gridLayout.addWidget(self.secondMailLabel, 2, 1, 1, 1)
+
+        self.purposeOfUseLabel = QLabel(CheckData)
+        self.purposeOfUseLabel.setObjectName(u"purposeOfUseLabel")
+        self.purposeOfUseLabel.setFont(font)
+
+        self.gridLayout.addWidget(self.purposeOfUseLabel, 4, 1, 1, 1)
+
+        self.label_12 = QLabel(CheckData)
+        self.label_12.setObjectName(u"label_12")
+
+        self.gridLayout.addWidget(self.label_12, 0, 2, 1, 1)
+
+        self.phoneNumberLabel = QLabel(CheckData)
+        self.phoneNumberLabel.setObjectName(u"phoneNumberLabel")
+        self.phoneNumberLabel.setFont(font)
+
+        self.gridLayout.addWidget(self.phoneNumberLabel, 2, 3, 1, 1)
+
+        self.label_29 = QLabel(CheckData)
+        self.label_29.setObjectName(u"label_29")
+
+        self.gridLayout.addWidget(self.label_29, 4, 0, 1, 1)
+
+        self.label = QLabel(CheckData)
+        self.label.setObjectName(u"label")
+
+        self.gridLayout.addWidget(self.label, 0, 0, 1, 1)
+
+        self.label_15 = QLabel(CheckData)
+        self.label_15.setObjectName(u"label_15")
+
+        self.gridLayout.addWidget(self.label_15, 1, 0, 1, 1)
+
+        self.label_5 = QLabel(CheckData)
+        self.label_5.setObjectName(u"label_5")
+
+        self.gridLayout.addWidget(self.label_5, 1, 2, 1, 1)
+
+        self.label_4 = QLabel(CheckData)
+        self.label_4.setObjectName(u"label_4")
+
+        self.gridLayout.addWidget(self.label_4, 1, 4, 1, 1)
+
+        self.label_23 = QLabel(CheckData)
+        self.label_23.setObjectName(u"label_23")
+
+        self.gridLayout.addWidget(self.label_23, 2, 4, 1, 1)
+
+        self.label_7 = QLabel(CheckData)
+        self.label_7.setObjectName(u"label_7")
+
+        self.gridLayout.addWidget(self.label_7, 3, 0, 1, 1)
+
+        self.label_18 = QLabel(CheckData)
+        self.label_18.setObjectName(u"label_18")
+
+        self.gridLayout.addWidget(self.label_18, 4, 2, 1, 1)
+
+        self.label_30 = QLabel(CheckData)
+        self.label_30.setObjectName(u"label_30")
+
+        self.gridLayout.addWidget(self.label_30, 2, 0, 1, 1)
+
+        self.label_10 = QLabel(CheckData)
+        self.label_10.setObjectName(u"label_10")
+
+        self.gridLayout.addWidget(self.label_10, 3, 2, 1, 1)
+
+        self.label_26 = QLabel(CheckData)
+        self.label_26.setObjectName(u"label_26")
+
+        self.gridLayout.addWidget(self.label_26, 2, 2, 1, 1)
+
+        self.mailLablel = QLabel(CheckData)
+        self.mailLablel.setObjectName(u"mailLablel")
+        self.mailLablel.setFont(font)
+
+        self.gridLayout.addWidget(self.mailLablel, 1, 1, 1, 1)
+
+        self.usernameLabel = QLabel(CheckData)
+        self.usernameLabel.setObjectName(u"usernameLabel")
+        self.usernameLabel.setFont(font)
+
+        self.gridLayout.addWidget(self.usernameLabel, 1, 3, 1, 1)
+
+        self.passwordLabel = QLabel(CheckData)
+        self.passwordLabel.setObjectName(u"passwordLabel")
+        self.passwordLabel.setFont(font)
+
+        self.gridLayout.addWidget(self.passwordLabel, 1, 5, 1, 1)
+
+        self.label_20 = QLabel(CheckData)
+        self.label_20.setObjectName(u"label_20")
+
+        self.gridLayout.addWidget(self.label_20, 4, 4, 1, 1)
+
+        self.mainDeviseLabel = QLabel(CheckData)
+        self.mainDeviseLabel.setObjectName(u"mainDeviseLabel")
+        self.mainDeviseLabel.setFont(font)
+
+        self.gridLayout.addWidget(self.mainDeviseLabel, 4, 5, 1, 1)
+
+        self.securityQuestionLabel = QLabel(CheckData)
+        self.securityQuestionLabel.setObjectName(u"securityQuestionLabel")
+        self.securityQuestionLabel.setFont(font)
+
+        self.gridLayout.addWidget(self.securityQuestionLabel, 4, 3, 1, 1)
+
+        self.gridLayout.setColumnStretch(0, 1)
+        self.gridLayout.setColumnStretch(1, 3)
+        self.gridLayout.setColumnStretch(2, 1)
+        self.gridLayout.setColumnStretch(3, 3)
+        self.gridLayout.setColumnStretch(4, 1)
+        self.gridLayout.setColumnStretch(5, 3)
+
+        self.verticalLayout.addLayout(self.gridLayout)
+
+        self.notePlainTextEdit = QPlainTextEdit(CheckData)
+        self.notePlainTextEdit.setObjectName(u"notePlainTextEdit")
+        self.notePlainTextEdit.setReadOnly(True)
+
+        self.verticalLayout.addWidget(self.notePlainTextEdit)
+
+        self.horizontalLayout = QHBoxLayout()
+        self.horizontalLayout.setObjectName(u"horizontalLayout")
+        self.horizontalSpacer = QSpacerItem(40, 20, QSizePolicy.Expanding, QSizePolicy.Minimum)
+
+        self.horizontalLayout.addItem(self.horizontalSpacer)
+
+        self.deletePushButton = QPushButton(CheckData)
+        self.deletePushButton.setObjectName(u"deletePushButton")
+
+        self.horizontalLayout.addWidget(self.deletePushButton)
+
+        self.modifyPushButton = QPushButton(CheckData)
+        self.modifyPushButton.setObjectName(u"modifyPushButton")
+
+        self.horizontalLayout.addWidget(self.modifyPushButton)
+
+        self.copyPasswordPushButton = QPushButton(CheckData)
+        self.copyPasswordPushButton.setObjectName(u"copyPasswordPushButton")
+
+        self.horizontalLayout.addWidget(self.copyPasswordPushButton)
+
+
+        self.verticalLayout.addLayout(self.horizontalLayout)
+
+        self.verticalLayout.setStretch(0, 3)
+        self.verticalLayout.setStretch(1, 5)
+        self.verticalLayout.setStretch(2, 1)
+
+        self.gridLayout_2.addLayout(self.verticalLayout, 0, 0, 1, 1)
+
+
+        self.retranslateUi(CheckData)
+
+        QMetaObject.connectSlotsByName(CheckData)
+    # setupUi
+
+    def retranslateUi(self, CheckData):
+        CheckData.setWindowTitle(QCoreApplication.translate("CheckData", u"Visualize Informations", None))
+        self.birthdateLabel.setText("")
+        self.websiteLabel.setText("")
+        self.linkedWebsiteLabel.setText("")
+        self.firstNameLabel.setText("")
+        self.label_11.setText(QCoreApplication.translate("CheckData", u"Linked website:", None))
+        self.label_17.setText(QCoreApplication.translate("CheckData", u"Answer:", None))
+        self.linkLabel.setText("")
+        self.lastNameLabel.setText("")
+        self.ansewerLabel.setText("")
+        self.secondMailLabel.setText("")
+        self.purposeOfUseLabel.setText("")
+        self.label_12.setText(QCoreApplication.translate("CheckData", u"Link:", None))
+        self.phoneNumberLabel.setText("")
+        self.label_29.setText(QCoreApplication.translate("CheckData", u"Purpose of Use:", None))
+        self.label.setText(QCoreApplication.translate("CheckData", u"Website:", None))
+        self.label_15.setText(QCoreApplication.translate("CheckData", u"E-mail:", None))
+        self.label_5.setText(QCoreApplication.translate("CheckData", u"UserName:", None))
+        self.label_4.setText(QCoreApplication.translate("CheckData", u"Password:", None))
+        self.label_23.setText(QCoreApplication.translate("CheckData", u"Birthdate:", None))
+        self.label_7.setText(QCoreApplication.translate("CheckData", u"First Name:", None))
+        self.label_18.setText(QCoreApplication.translate("CheckData", u"Security Question:", None))
+        self.label_30.setText(QCoreApplication.translate("CheckData", u"Second E-mail:", None))
+        self.label_10.setText(QCoreApplication.translate("CheckData", u"Last Name:", None))
+        self.label_26.setText(QCoreApplication.translate("CheckData", u"Phone Number:", None))
+        self.mailLablel.setText("")
+        self.usernameLabel.setText("")
+        self.passwordLabel.setText("")
+        self.label_20.setText(QCoreApplication.translate("CheckData", u"Main Device:", None))
+        self.mainDeviseLabel.setText("")
+        self.securityQuestionLabel.setText("")
+        self.notePlainTextEdit.setPlaceholderText(QCoreApplication.translate("CheckData", u"Notes", None))
+        self.deletePushButton.setText(QCoreApplication.translate("CheckData", u"Delete", None))
+        self.modifyPushButton.setText(QCoreApplication.translate("CheckData", u"Modify", None))
+        self.copyPasswordPushButton.setText(QCoreApplication.translate("CheckData", u"Copy Password", None))
+    # retranslateUi
+
