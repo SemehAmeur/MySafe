ac92bdc90c2f222dab6af287621ca6af2c539dc6
SemehAmeur <semeh.ameur@gmail.com>
2024-09-20 11:18:17 +0200

    login: myshow, connect_btn func

--- login.py
+++ login.py
@@ -1,8 +1,13 @@
+import hashlib
+
+from PySide6.QtWidgets import QWidget, QMessageBox
+
 import MainWindow
 from create_account import CreateAccount
 from UI.ui_login import Ui_Login
 
 import sqlite3
+
 
 class Login(QWidget, Ui_Login):
     def __init__(self, app):
@@ -15,12 +20,72 @@ class Login(QWidget, Ui_Login):
         self.user = self.usernameLineEdit.text()
         self.createAccountPushButton.clicked.connect(self.create_account)
         self.loginPushButton.clicked.connect(self.connect_btn)
-    
+
     def create_account(self):
         self.createaccount = CreateAccount(self.app)
         self.createaccount.show()
-        
-    def myshow(self): 
-        pass
-    def connect_btn(self): 
-        pass
+
+    def myshow(self):
+        self.show()
+
+    def connect_btn(self):
+        # Empty fields
+        if self.usernameLineEdit.text() == "" and self.passwordLineEdit.text() == "":
+            ret = QMessageBox.warning(self, 'Missing fields',
+                                      'insert the username and password',
+                                      QMessageBox.Ok | QMessageBox.Close)
+            if ret == QMessageBox.Close:
+                self.close()
+
+        # Empty username only
+        elif self.usernameLineEdit.text() == "":
+            ret = QMessageBox.warning(self, 'Missing field',
+                                      'Type in your username',
+                                      QMessageBox.Ok | QMessageBox.Close)
+            if ret == QMessageBox.Close:
+                self.close()
+
+        # Empty Password only
+        elif self.passwordLineEdit.text() == "":
+            ret = QMessageBox.warning(self, 'Missing fields',
+                                      'Type in your password',
+                                      QMessageBox.Ok | QMessageBox.Close)
+            if ret == QMessageBox.Close:
+                self.close()
+
+        else:
+            try:
+                statement = 'SELECT user_password FROM users WHERE user_name = "' + self.usernameLineEdit.text() + '"'
+                connection = sqlite3.connect("safeconfig.db")
+                cursor = connection.cursor()
+
+                cursor.execute(statement)
+                output = cursor.fetchall()
+                connection.commit()
+                connection.close()
+
+                # Store the password into a variable to make it less writing
+                input_pass = self.passwordLineEdit.text()
+
+                if hashlib.sha256(input_pass.encode()).hexdigest() == output[0][0]:
+
+                    self.mymain = MainWindow.MainWindow(self.app, self.usernameLineEdit.text(), self.usernameLineEdit.text(), self)
+
+                    self.hide()
+                    self.usernameLineEdit.setText('')
+                    self.passwordLineEdit.setText('')
+
+
+                else:
+                    QMessageBox.critical(self, 'Invalid Information',
+                                         'Username or Password wes/were incorrect',
+                                         QMessageBox.Ok)
+                    self.usernameLineEdit.setText('')
+                    self.passwordLineEdit.setText('')
+
+            except:
+                QMessageBox.critical(self, 'Invalid Information',
+                                     'Username or Password wes/were incorrect',
+                                     QMessageBox.Ok)
+                self.usernameLineEdit.setText('')
+                self.passwordLineEdit.setText('')
