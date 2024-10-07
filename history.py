08554e12d415c6b90f756f47b05ed8be3d8ec8e9
SemehAmeur <semeh.ameur@gmail.com>
2024-10-07 10:15:40 +0200

    history: preview_history func

--- history.py
+++ history.py
@@ -13,3 +13,24 @@ class History(QWidget, Ui_History):
         self.app = app
         self.show()
         self.username = username
+
+        self.preview_history()
+
+    def preview_history(self):
+        print(self.username)
+        stamtment = '''SELECT * FROM activities WHERE user_name = ?'''
+        connection = sqlite3.connect('safeconfig.db')
+        cursor = connection.cursor()
+        param = tuple(self.username)
+        cursor.execute(stamtment, param)
+        output = cursor.fetchall()
+        connection.commit()
+        connection.close()
+        for el in output:
+            row_position = self.tableWidget.rowCount()
+            self.tableWidget.insertRow(row_position)
+            self.tableWidget.setItem(row_position, 0, QTableWidgetItem((el[1])))
+            self.tableWidget.setItem(row_position, 1, QTableWidgetItem((el[2])))
+            self.tableWidget.setItem(row_position, 2, QTableWidgetItem((el[3])))
+            self.tableWidget.setItem(row_position, 3, QTableWidgetItem((el[4])))
+
