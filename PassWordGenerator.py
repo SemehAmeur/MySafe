5f9752fe43ec2e6e3344734e76b4fa65c08de17c
SemehAmeur <semeh.ameur@gmail.com>
2024-10-08 08:41:50 +0200

    passwordgenerator: charlistfunc func

--- PassWordGenerator.py
+++ PassWordGenerator.py
@@ -3,7 +3,24 @@ import random
 
 
 class PassWordGenerator:
-    def charlistfunc(self):
-        pass
+        def charlistfunc(self, lowercase_char, uppercase_char, numbers_char, specialdigit_char):
+        characterList = ""
+
+        # Adding uppercase letters to possible characters
+        if uppercase_char:
+            characterList += string.ascii_uppercase
+
+        # Adding lowercase letters to possible characters
+        if lowercase_char:
+            characterList += string.ascii_lowercase
+
+        # Adding digits to possible characters
+        if numbers_char:
+            characterList += string.digits
+
+        # Adding special characters to possible
+        if specialdigit_char:
+            characterList += string.punctuation
+        return characterList
     def passgenerate():
         pass
